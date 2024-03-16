32f78517d2ab97cf5449491d5938d84ba5b1df00
lynx6625 <kaneki6625@gmail.com>
2024-03-16 23:14:00 +0530

    Cleared repo for upload

--- Javascipt/Fibonacci.js
+++
@@ -1,9 +0,0 @@
-var a = 1, b = 1, f;
-/* complete this "for" loop*/
-for(let i = 3; i<=15; i++){   //technically 1 and 1 are 2nd and 3rd fibonacci cause fibonacci starts from zero
-    f = a+b;                  // f is result var
-    a=b;                      //shifting a and b forward by one position
-    b=f;                      
-}
-
-console.log(f)
