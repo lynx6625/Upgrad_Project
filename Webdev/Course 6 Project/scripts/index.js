c48fe3d134ffee981adfb6981daa926355fc6959
lynx6625 <kaneki6625@gmail.com>
2024-03-16 14:04:20 +0530

    page 2

--- Webdev/Course 6 Project/scripts/index.js
+++ Webdev/Course 6 Project/scripts/index.js
@@ -1,19 +1,76 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var modal = document.getElementById("myModal");
-    var btn = document.getElementById("signup");
-    var span = document.getElementsByClassName("close")[0];
+    var signupModal = document.getElementById("signupModal");
+    var signinModal = document.getElementById("signinModal");
+    var cpModal = document.getElementById("cpModal");
 
-    btn.onclick = function() {
-      modal.style.display = "flex";
+    var signupBtn = document.getElementById("signup");
+    var signinBtn = document.getElementById("signin");
+    var cpBtn = document.getElementById("cp");
+
+    var signupSpan = signupModal.querySelector(".close");
+    var signinSpan = signinModal.querySelector(".close2");
+    var cpSpan = cpModal.querySelector(".close3");
+
+    // Function to open sign-up modal
+    function openSignup() {
+        signupModal.style.display = "flex";
     }
 
-    span.onclick = function() {
-      modal.style.display = "none";
+    // Function to close sign-up modal
+    function closeSignup() {
+        signupModal.style.display = "none";
     }
 
+    // Function to open sign-in modal
+    function openSignin() {
+        signinModal.style.display = "flex";
+    }
+
+    // Function to close sign-in modal
+    function closeSignin() {
+        signinModal.style.display = "none";
+    }
+
+    function opencp() {
+        cpModal.style.display = "flex";
+    }
+    function closecp() {
+        cpModal.style.display = "none";
+    }
+
+    // Event listener for sign-up button
+    signupBtn.onclick = openSignup;
+
+    // Event listener for close button on sign-up modal
+    signupSpan.onclick = closeSignup;
+
+    // Event listener for sign-in button
+    signinBtn.onclick = openSignin;
+
+    // Event listener for close button on sign-in modal
+    signinSpan.onclick = closeSignin;
+
+    cpBtn.onclick = opencp;
+    cpSpan.onclick = closecp;
+
+    if(signupSpan) signupSpan.onclick = closeSignup;
+    if(signinSpan) signinSpan.onclick = closeSignin;
+
+    
     window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+        if (event.target == signupModal) {
+            closeSignup();
+        } else if (event.target == signinModal) {
+            closeSignin();
+        }
+    };
+
+    var switchToSignupLinks = document.querySelectorAll(".switch-to-signup");
+    switchToSignupLinks.forEach(function(link) {
+        link.onclick = function() {
+            closeSignin();
+            openSignup();
+            return false;
+        };
+    });
 });
